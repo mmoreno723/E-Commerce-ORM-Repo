c92250f81851567a5f6e11e18d5527b04829acc9
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-06-16 16:39:37 -0400

    added sequelize connection to server.js file

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
-  procees.env.DB_USER,
+  process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: 'localhost',
